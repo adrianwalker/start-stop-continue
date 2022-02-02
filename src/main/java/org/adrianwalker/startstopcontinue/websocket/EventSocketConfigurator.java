537e07f12e446215b2d7f142da7b864baec8b87d
Adrian Walker <adrian.walker@bcs.org>
2022-02-02 22:12:47 +0000

    Fix for EventSocket never getting garbage collected, so only ever creating one instance

--- src/main/java/org/adrianwalker/startstopcontinue/websocket/EventSocketConfigurator.java
+++ src/main/java/org/adrianwalker/startstopcontinue/websocket/EventSocketConfigurator.java
@@ -1,22 +1,24 @@
 package org.adrianwalker.startstopcontinue.websocket;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.adrianwalker.startstopcontinue.pubsub.EventPubSub;
 import javax.websocket.server.ServerEndpointConfig;
 
 public final class EventSocketConfigurator extends ServerEndpointConfig.Configurator {
-
-  private final EventPubSub eventPubSub;
+  
+  private final Map<Class, Object> endPointInstances = new HashMap<>(); 
 
   public EventSocketConfigurator(final EventPubSub eventPubSub) {
 
     super();
 
-    this.eventPubSub = eventPubSub;
+    endPointInstances.put(EventSocket.class, new EventSocket(eventPubSub));
   }
 
   @Override
   public <T> T getEndpointInstance(final Class<T> clazz) throws InstantiationException {
 
-    return (T) new EventSocket(eventPubSub);
+    return (T) endPointInstances.get(clazz);
   }
 }
